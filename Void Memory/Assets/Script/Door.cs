fc5a8d22b6ce7e3903b703664e3dde8a7f068f50
Deliss <morganebris@gmail.com>
2016-11-04 00:36:08 -0400

    Début environement

---
+++ Void Memory/Assets/Script/Door.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour
+{
+   // public Vector3 OpenPosition;
+    private bool open;
+
+    public IEnumerator OpenUp()
+    {
+        Vector3 OpenPosition = transform.position + new Vector3(0, 2, 0);
+        if (!open)
+        {
+            while (transform.position != OpenPosition)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, OpenPosition, 0.05f);
+
+                if (Vector3.Distance(transform.position, OpenPosition) <= 0.25f)
+                {
+                    transform.position = OpenPosition;
+                    open = true;
+                }
+                yield return null;
+            }
+        }
+    }
+}
