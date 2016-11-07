26c6cfdca587559fd60b0b09c4f4a8ae940ab45a
Bimèche <leo.gar@hotmail.fr>
2016-11-06 20:25:31 -0500

    Commit Buzz + Horcan

---
+++ Void Memory/Assets/Standard Assets/2D/Scripts/BuzzController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityStandardAssets._2D
+{
+	public class BuzzController : PlatformerCharacter2D
+	{
+		private float m_timerJetpack = 0f;
+		private bool m_jetpackActive = false;
+		
+		private void FixedUpdate()
+		{
+			m_Grounded = false;
+
+			// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+			// This can be done using layers instead but Sample Assets will not overwrite your project settings.
+			Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+			for (int i = 0; i < colliders.Length; i++)
+			{
+				if (colliders[i].gameObject != gameObject && !m_jetpackActive)
+					m_Grounded = true;
+			}
+			m_Anim.SetBool("Ground", m_Grounded);
+
+			// Set the vertical animation
+			m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
+			if (!m_Jump)
+			{
+				// Read the jump input in Update so button presses aren't missed.
+				m_Jump = Input.GetButtonDown("JumpBuzz");
+			}
+		}
+
+
+		public override void Move()
+		{
+			// Read the inputs.
+			bool crouch = Input.GetKey(KeyCode.RightControl);
+			float move = Input.GetAxis("HorizontalBuzz");
+			// If crouching, check to see if the character can stand up
+			if (!crouch && m_Anim.GetBool("Crouch"))
+			{
+				// If the character has a ceiling preventing them from standing up, keep them crouching
+				if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+				{
+					crouch = true;
+				}
+			}
+
+			// Set whether or not the character is crouching in the animator
+			m_Anim.SetBool("Crouch", crouch);
+
+			//only control the player if grounded or airControl is turned on
+			if (m_Grounded || m_AirControl)
+			{
+				if (m_Grounded)
+				{
+					m_timerJetpack = 0f;
+					m_jetpackActive = false;
+				}
+				// Reduce the speed if crouching by the crouchSpeed multiplier
+				move = (crouch ? move * m_CrouchSpeed : move);
+
+				// The Speed animator parameter is set to the absolute value of the horizontal input.
+				m_Anim.SetFloat("Speed", Mathf.Abs(move));
+
+				// Move the character
+				if (Input.GetKey(KeyCode.J) && m_jetpackActive)
+					m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, 0f);
+				else
+					m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+
+				// If the input is moving the player right and the player is facing left...
+				if (move > 0 && !m_FacingRight)
+				{
+					// ... flip the player.
+					Flip();
+				}
+				// Otherwise if the input is moving the player left and the player is facing right...
+				else if (move < 0 && m_FacingRight)
+				{
+					// ... flip the player.
+					Flip();
+				}
+			}
+			if (Input.GetKey(KeyCode.J) && m_timerJetpack < 1f)
+			{
+				m_Rigidbody2D.velocity.Set(0f, 0f);
+				m_Rigidbody2D.gravityScale = 0;
+				m_timerJetpack += 0.01f;
+				print(m_timerJetpack);
+				m_jetpackActive = true;
+				m_Grounded = false;
+				m_Anim.SetBool("Ground", false);
+			}
+			else
+			{
+				m_jetpackActive = false;
+				m_Rigidbody2D.gravityScale = 3;
+			}
+			// If the player should jump...
+			if (m_Grounded && m_Jump && m_Anim.GetBool("Ground"))
+			{
+				// Add a vertical force to the player.
+				m_Grounded = false;
+				m_Anim.SetBool("Ground", false);
+				m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+			}
+			m_Jump = false;
+		}
+	}
+}
--- Void Memory/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Void Memory/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -7,7 +7,6 @@ namespace UnityStandardAssets._2D
     public class Platformer2DUserControl : MonoBehaviour
     {
         private PlatformerCharacter2D m_Character;
-        private bool m_Jump;
 
 
         private void Awake()
@@ -18,22 +17,13 @@ namespace UnityStandardAssets._2D
 
         private void Update()
         {
-            if (!m_Jump)
-            {
-                // Read the jump input in Update so button presses aren't missed.
-                m_Jump = Input.GetButtonDown("Jump");
-            }
         }
 
 
         private void FixedUpdate()
         {
-            // Read the inputs.
-            bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = Input.GetAxis("Horizontal");
             // Pass all parameters to the character control script.
-            m_Character.Move(h, crouch, m_Jump);
-            m_Jump = false;
+            m_Character.Move();
         }
 
 		void OnTriggerEnter(Collider other)
