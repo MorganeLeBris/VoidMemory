e3a4028eed29337d7e463722219c90e29a1e74e7
JoannaSassano <joannasassano@hotmail.fr>
2016-11-22 15:25:52 -0500

    DernièrePartieHistoire
    Rajout du dernier dialogue, celui avant le shooter

--- Assets/Scripts/AffichageHist2.cs
+++ Assets/Scripts/AffichageHist2.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AffichageHist2 : MonoBehaviour
 {
@@ -9,8 +10,10 @@ public class AffichageHist2 : MonoBehaviour
     private int i = 0;
     private int j = 2;
     private string strComplete;
+    public AudioSource voix;
     void Start()
     {
+        voix.Play();
         strComplete = "Alors que Buzz admirait paisiblement le ciel, une météorite passa droit devant ses yeux. C'était magnifique et à la fois inquiétant. La curiosité était une des plus grandes qualités de Buzz et à la fois son plus grand défaut. Sans perdre une minute il enfila ses chaussures et partit en direction de la météorite qui à la vue de sa grosseur dans le ciel n'avait pas du tomber bien loin. Ce n'est qu'après une heure de marche qu'un cratère au loin commençait à prendre forme.";
     }
 
@@ -28,6 +31,20 @@ public class AffichageHist2 : MonoBehaviour
         else
         {
             j++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(i< strComplete.Length)
+            {
+                Text.text = strComplete;
+                voix.Stop();
+                i = strComplete.Length;
+            }else
+            {
+                SceneManager.LoadScene(2);
+            }
+            
         }
 
     }
