5f2a5a13892f76c61853aea680518e2e90523218
Deliss <morganebris@gmail.com>
2016-11-24 17:53:27 -0500

    test

---
+++ Assets/ScalingScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScalingScript : MonoBehaviour {
+
+    public Camera maincamera;
+
+	// Use this for initialization
+	void Start () {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        transform.localScale = maincamera.scalingFactor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
